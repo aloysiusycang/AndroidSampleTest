4a549be7059d8d1ab6f7ee2717ed69f67921be6f
aloysiusycang <y.c.ang_aloysius@hotmail.com>
2014-01-16 11:37:15 +0800

    changes

--- ActivityInstrumentationSample/src/main/java/com/example/android/activityinstrumentation/MainActivity.java
+++ ActivityInstrumentationSample/src/main/java/com/example/android/activityinstrumentation/MainActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.activityinstrumentationTest;
+package com.example.android.activityinstrumentation;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
